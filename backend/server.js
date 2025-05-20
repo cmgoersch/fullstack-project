ad17be5a5a5b3b31a3c93ead9a79a66b453f8e49
cmgoersch <goersch@madways.de>
2025-05-20 14:33:11 +0200

    post methode changed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import users from './data/users.json' with {type: "json"}
 import todos from './data/todos.json'  with {type: "json"}
+import cors from "cors";
+
 
 
 const app = express()
@@ -8,14 +10,22 @@ const PORT = 5500
 
 // middleware
 app.use(express.json())
+app.use(cors());
 
 // app.get('/', (req, res) => res.send('Hallo World'))
 
-app.post("/login", (req, res)=>{
-    const {username, password} = req.body
-    const newUser = users.find((user)=> user.username === username && user.password === password) 
-    res.json(newUser)
-})
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (user) {
+    const { password, ...safeUser } = user;
+    res.json(safeUser); 
+  } else {
+    res.status(401).json({ message: "Login fehlgeschlagen" });
+  }
+});
 
 app.post("/todos", (req, res)=>{
     const {userID} =req.body
