e185482018bdabe14e2e0b66038fa247e5507f46
cmgoersch <goersch@madways.de>
2025-05-21 09:36:13 +0200

    some styling

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Welcome.css";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
-  )
-}
+    <div className="welcome-container">
+      <h1>👋 Willkommen bei <span className="app-name">Where is my data?</span></h1>
+      <p className="subtitle">Dein persönliches Todo-Dashboard</p>
+      <p className="hint">Bitte melde dich an, um deine Todos zu sehen.</p>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
